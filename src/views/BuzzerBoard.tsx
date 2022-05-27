66d488e3d548e4ee0fdf4bbfae7391fdceb6ec4f
Daniel Luca <hey@danielluca.com>
2022-05-27 23:50:15 +0200

    fix: mobile view

--- src/views/BuzzerBoard.tsx
+++ src/views/BuzzerBoard.tsx
@@ -3,7 +3,7 @@ import sounds from "../sounds";
 
 function BuzzerBoard() {
 	return (
-		<main className="grid grid-cols-3 gap-4 p-24">
+		<main className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-8 md:16 lg:p-24">
 			{sounds.map((sound, i) => {
 				return <Buzzer title={sound.title} soundFile={sound.file} key={i} />;
 			})}
