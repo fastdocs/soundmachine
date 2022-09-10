3b414de0ca9dec0362875247d923bcfb0f64d10f
Daniel Luca <hey@danielluca.com>
2022-09-10 21:45:47 +0200

    refactor: moving files into subdirectories

--- src/soundfiles/index.ts
+++ src/soundfiles/index.ts
@@ -1,7 +1,7 @@
 import getFile from "../utility/getFile";
 
 type SoundCategory = {
-	categoryTitle: string;
+	title: string;
 	sounds: Sounds[];
 };
 
@@ -12,252 +12,252 @@ type Sounds = {
 
 const sounds: SoundCategory[] = [
 	{
-		categoryTitle: "Ronny",
+		title: "LKW Ali",
 		sounds: [
-			{
-				title: "Die Scheiße",
-				file: getFile("die-scheisse"),
-			},
-			{
-				title: "Es reicht langsam!",
-				file: getFile("es-reicht-langsam"),
-			},
-			{
-				title: "Kein Bandmaß!?",
-				file: getFile("kein-bandmass"),
-			},
-			{
-				title: "Kein Lust oder wat!?",
-				file: getFile("keine-lust"),
-			},
-			{
-				title: "Nach Hause gehen",
-				file: getFile("nach-hause"),
-			},
-			{
-				title: "Nichts-Könner",
-				file: getFile("nichts-koenner"),
-			},
-			{
-				title: "Zu dumm?",
-				file: getFile("zu-dumm"),
-			},
-		],
-	},
-	{
-		categoryTitle: "LKW-Ali",
-		sounds: [
-			{
-				title: "Besser su hause bleiben!",
-				file: getFile("besser-zu-hause"),
-			},
 			{
 				title: "Warum Straße!?",
-				file: getFile("warum-strasse"),
+				file: getFile("lkw-ali/warum-strasse"),
+			},
+			{
+				title: "Besser su hause bleiben!",
+				file: getFile("lkw-ali/besser-zu-hause"),
 			},
 			{
 				title: "Verpiss dik!",
-				file: getFile("verpiss-dik"),
+				file: getFile("lkw-ali/verpiss-dik"),
 			},
 		],
 	},
 	{
-		categoryTitle: "Wolle Marco",
+		title: "Krankplatz Ronny",
+		sounds: [
+			{
+				title: "Die Scheiße",
+				file: getFile("krankplatz-ronny/die-scheisse"),
+			},
+			{
+				title: "Kein Lust oder wat!?",
+				file: getFile("krankplatz-ronny/keine-lust"),
+			},
+			{
+				title: "Nach Hause gehen",
+				file: getFile("krankplatz-ronny/nach-hause"),
+			},
+			{
+				title: "Zu dumm?",
+				file: getFile("krankplatz-ronny/zu-dumm"),
+			},
+			{
+				title: "Nichts-Könner",
+				file: getFile("krankplatz-ronny/nichts-koenner"),
+			},
+			{
+				title: "Es reicht langsam!",
+				file: getFile("krankplatz-ronny/es-reicht-langsam"),
+			},
+			{
+				title: "Kein Bandmaß!?",
+				file: getFile("krankplatz-ronny/kein-bandmass"),
+			},
+		],
+	},
+	{
+		title: "Wolle Marco",
 		sounds: [
 			{
 				title: "5G brauchen wir!",
-				file: getFile("5G"),
+				file: getFile("wolle-marco/5G"),
 			},
 			{
 				title: "Chai Latte",
-				file: getFile("chai_latte"),
+				file: getFile("wolle-marco/chai_latte"),
 			},
-			{ title: "Es lassen", file: getFile("es_lassen") },
+			{ title: "Es lassen", file: getFile("wolle-marco/es_lassen") },
 			{
 				title: "Dat gleiche Feld",
-				file: getFile("dat_gleiche_feld"),
+				file: getFile("wolle-marco/dat_gleiche_feld"),
 			},
 			{
 				title: "Ich brauche Platz!",
-				file: getFile("ich_brauche_platz"),
+				file: getFile("wolle-marco/ich_brauche_platz"),
 			},
 		],
 	},
 	{
-		categoryTitle: "Bitconnect",
+		title: "Bitconnect",
 		sounds: [
 			{
 				title: "Bitconnect (No No No)",
-				file: getFile("Bitconnect_No_No"),
+				file: getFile("bitconnect/Bitconnect_No_No"),
 			},
 			{
 				title: "Bitconnect (Waz Up)",
-				file: getFile("WazUp"),
+				file: getFile("bitconnect/WazUp"),
 			},
 			{
 				title: "Bitconnect (Hey hey)",
-				file: getFile("heyhey"),
+				file: getFile("bitconnect/heyhey"),
 			},
 		],
 	},
 	{
-		categoryTitle: "Fußball Misc",
+		title: "Fußball Misc",
 		sounds: [
 			{
 				title: "CR7 (Siuu)",
-				file: getFile("cr7_siuu"),
+				file: getFile("fussball-misc/cr7_siuu"),
 			},
 			{
 				title: "Woran hat et jelegen?",
-				file: getFile("woran-hats-gelegen"),
+				file: getFile("fussball-misc/woran-hats-gelegen"),
 			},
 		],
 	},
 	{
-		categoryTitle: "Misc",
+		title: "Misc",
 		sounds: [
 			{
 				title: "Ich muss raus",
-				file: getFile("ich-muss-raus"),
+				file: getFile("misc/ich-muss-raus"),
 			},
 			{
 				title: "Ich hab ganis gemakt",
-				file: getFile("ich_hab_ganis_gemakt"),
+				file: getFile("misc/ich_hab_ganis_gemakt"),
 			},
 			{
 				title: "Nicht so tief Rüdiger",
-				file: getFile("nicht-so-tief-rudiger"),
+				file: getFile("misc/nicht-so-tief-rudiger"),
 			},
 			{
 				title: "Okay let's go",
-				file: getFile("okay-lets-go"),
+				file: getFile("misc/okay-lets-go"),
 			},
 			{
 				title: "TS - User disconnected",
-				file: getFile("teamspeak-disconnect"),
+				file: getFile("misc/teamspeak-disconnect"),
 			},
 			{
 				title: "Uwe ist auch dabei",
-				file: getFile("ich-bin-der-uwe-ich-bin-auch-dabei"),
+				file: getFile("misc/ich-bin-der-uwe-ich-bin-auch-dabei"),
 			},
 			{
 				title: "Was ist mit Karsten los",
-				file: getFile("was-ist-denn-mit-karsten-los"),
+				file: getFile("misc/was-ist-denn-mit-karsten-los"),
 			},
 			{
 				title: "Risiken und Nebenwirkungen",
-				file: getFile("zu-risiken-und-nebenwirkungen"),
+				file: getFile("misc/zu-risiken-und-nebenwirkungen"),
 			},
 			{
 				title: "Do u speak germany?",
-				file: getFile("do-you-speak-germany"),
+				file: getFile("misc/do-you-speak-germany"),
 			},
 			{
 				title: "Nice!",
-				file: getFile("click-nice"),
+				file: getFile("misc/click-nice"),
 			},
 			{
 				title: "Computer sagt NEIN",
-				file: getFile("computer-sagt-nein"),
+				file: getFile("misc/computer-sagt-nein"),
 			},
 			{
 				title: "There is the flavour!",
-				file: getFile("fuckin-flavour"),
+				file: getFile("misc/fuckin-flavour"),
 			},
 			{
 				title: "Psycho Violine",
-				file: getFile("psycho-violine"),
+				file: getFile("misc/psycho-violine"),
 			},
 			{
 				title: "Merkel du Fresse",
-				file: getFile("angela-merkel-du-fresse"),
+				file: getFile("misc/angela-merkel-du-fresse"),
 			},
 			{
 				title: "Shisha soll auf sein!",
-				file: getFile("shisha-soll-auf-sein"),
+				file: getFile("misc/shisha-soll-auf-sein"),
 			},
 		],
 	},
 	{
-		categoryTitle: "Jingles",
+		title: "Jingles",
 		sounds: [
 			{
 				title: "Wer wird Millionär",
-				file: getFile("wer-wird-millionar"),
+				file: getFile("jingles/wer-wird-millionar"),
 			},
 			{
 				title: "Wartemusik",
-				file: getFile("wartemusik-trompete"),
+				file: getFile("jingles/wartemusik-trompete"),
 			},
 			{
 				title: "Der Looser",
-				file: getFile("einer-ist-immer-der-loser"),
+				file: getFile("jingles/einer-ist-immer-der-loser"),
 			},
 			{
 				title: "Feierabend wie das duftet...",
-				file: getFile("feierabend-pommersche"),
+				file: getFile("jingles/feierabend-pommersche"),
 			},
 		],
 	},
 	{
-		categoryTitle: "Christian Streich",
+		title: "Christian Streich",
 		sounds: [
 			{
 				title: "Mo. zu Mit.",
-				file: getFile("mon-zu-mit"),
+				file: getFile("christian-streich/mon-zu-mit"),
 			},
 			{
 				title: "So. zu Di.",
-				file: getFile("Son-zu-die"),
+				file: getFile("christian-streich/Son-zu-die"),
 			},
 			{
 				title: "Fr. Do. Sa.",
-				file: getFile("fre-don-sam"),
+				file: getFile("christian-streich/fre-don-sam"),
 			},
 			{
 				title: "Heute ruhig",
-				file: getFile("heute-ruhig"),
+				file: getFile("christian-streich/heute-ruhig"),
 			},
 			{
 				title: "Verstehen Sie?",
-				file: getFile("verstehen-sie"),
+				file: getFile("christian-streich/verstehen-sie"),
 			},
 		],
 	},
 	{
-		categoryTitle: "Robert",
+		title: "Robert",
 		sounds: [
 			{
 				title: "Abitur",
-				file: getFile("abitur"),
+				file: getFile("robert-geiss/abitur"),
 			},
 			{
 				title: "Ende aus",
-				file: getFile("ende-aus-nikolaus"),
+				file: getFile("robert-geiss/ende-aus-nikolaus"),
 			},
 			{
 				title: "Ernste Lage",
-				file: getFile("ernste-lage"),
+				file: getFile("robert-geiss/ernste-lage"),
 			},
 			{
 				title: "Ha, ha, ha",
-				file: getFile("ha-ha-ha"),
+				file: getFile("robert-geiss/ha-ha-ha"),
 			},
 			{
 				title: "Prost!",
-				file: getFile("prost"),
+				file: getFile("robert-geiss/prost"),
 			},
 			{
 				title: "Regeln befolgen!",
-				file: getFile("regeln-befolgen"),
+				file: getFile("robert-geiss/regeln-befolgen"),
 			},
 			{
 				title: "Ski-Urlaub abgesagt",
-				file: getFile("ski-urlaub-abgesagt"),
+				file: getFile("robert-geiss/ski-urlaub-abgesagt"),
 			},
 			{
 				title: "Spaghetti Carbonara",
-				file: getFile("spaghettie-carbonara"),
+				file: getFile("robert-geiss/spaghettie-carbonara"),
 			},
 		],
 	},
